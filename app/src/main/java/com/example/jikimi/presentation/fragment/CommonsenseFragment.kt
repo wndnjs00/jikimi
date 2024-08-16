7b622b9d43ae2c22db6c97b4ada2470e769450a9
wndnjs00 <juwonjeon852@gmail.com>
2024-08-17 05:23:44 +0900

    package: 패키지구조 일부수정

--- app/src/main/java/com/example/jikimi/presentation/fragment/CommonsenseFragment.kt
+++ app/src/main/java/com/example/jikimi/presentation/fragment/CommonsenseFragment.kt
@@ -1,4 +1,4 @@
-package com.example.jikimi
+package com.example.jikimi.presentation.fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.jikimi.databinding.FragmentCommonsenseBinding
-import com.example.jikimi.databinding.FragmentEvacuateBinding
 
 
 class CommonsenseFragment : Fragment() {
--- app/src/main/java/com/example/jikimi/presentation/fragment/EvacuateFragment.kt
+++ app/src/main/java/com/example/jikimi/presentation/fragment/EvacuateFragment.kt
@@ -1,4 +1,4 @@
-package com.example.jikimi
+package com.example.jikimi.presentation.fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
