85cb4162747d99d1b52c0a91315a7144c672af52
wndnjs00 <juwonjeon852@gmail.com>
2024-08-17 05:14:11 +0900

    package: 전체적인 패키지구조 생성

--- app/src/main/java/com/example/jikimi/presentation/MainActivity.kt
+++ app/src/main/java/com/example/jikimi/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.jikimi
+package com.example.jikimi.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -6,6 +6,7 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
+import com.example.jikimi.R
 import com.example.jikimi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
