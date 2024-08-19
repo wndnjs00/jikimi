9179ab2822382f71b6c0e2ec615fe5ebea52407b
wndnjs00 <juwonjeon852@gmail.com>
2024-08-19 17:02:05 +0900

    feat: 위치권한부여, 현재위치받아오기

--- app/src/main/java/com/example/jikimi/presentation/fragment/EvacuateFragment.kt
+++ app/src/main/java/com/example/jikimi/presentation/fragment/EvacuateFragment.kt
@@ -5,13 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.jikimi.R
 import com.example.jikimi.databinding.FragmentEvacuateBinding
+import com.naver.maps.map.LocationTrackingMode
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.util.FusedLocationSource
 
 
-class EvacuateFragment : Fragment() {
+class EvacuateFragment : Fragment(), OnMapReadyCallback {
     private val binding get() = _binding!!
     private var _binding: FragmentEvacuateBinding? = null
-
+    private lateinit var locationSource: FusedLocationSource
+    private lateinit var naverMap: NaverMap
 
 
     override fun onCreateView(
@@ -25,10 +32,55 @@ class EvacuateFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initializeMap()
+        initializeLocationSource()
     }
 
 
+    // 지도 초기화
+    private fun initializeMap() {
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                childFragmentManager.beginTransaction().add(R.id.map, it).commit()
+            }
+        // 지도가 준비되었을때 onMapReady콜백 받도록
+        mapFragment.getMapAsync(this)
+    }
 
+
+    // FusedLocationSource 초기화
+    private fun initializeLocationSource() {
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+    }
+
+
+    @Deprecated("Deprecated in Java")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
+            // 권한 거부한경우
+            if (!locationSource.isActivated) {
+                naverMap.locationTrackingMode = LocationTrackingMode.None //위치추적 비활성화
+            }
+            return
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onMapReady(naverMap: NaverMap) {
+        this.naverMap = naverMap
+        naverMap.locationSource = locationSource   //현재위치
+        naverMap.uiSettings.isLocationButtonEnabled = true  //현재위치 버튼 활성화
+        naverMap.locationTrackingMode = LocationTrackingMode.Follow //현재위치를 따라 움직임
+    }
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
