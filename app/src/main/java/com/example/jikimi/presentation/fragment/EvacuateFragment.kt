12138f1475f5e085031a299eb6992edfaa2616f8
wndnjs00 <juwonjeon852@gmail.com>
2024-09-29 23:10:28 +0900

    design: 검색,좋아요 xml 디자인

--- app/src/main/java/com/example/jikimi/presentation/fragment/EvacuateFragment.kt
+++ app/src/main/java/com/example/jikimi/presentation/fragment/EvacuateFragment.kt
@@ -7,8 +7,10 @@ import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
