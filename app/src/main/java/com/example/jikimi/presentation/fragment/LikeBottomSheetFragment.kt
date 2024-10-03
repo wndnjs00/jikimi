e9afc896212edfcf31a1e40e17f004c9d708c833
wndnjs00 <juwonjeon852@gmail.com>
2024-10-03 17:30:56 +0900

    feat: LikeBottomSheetFragment 롱클릭 적용

--- app/src/main/java/com/example/jikimi/presentation/fragment/LikeBottomSheetFragment.kt
+++ app/src/main/java/com/example/jikimi/presentation/fragment/LikeBottomSheetFragment.kt
@@ -1,15 +1,20 @@
 package com.example.jikimi.presentation.fragment
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.jikimi.R
 import com.example.jikimi.data.model.entity.LikeEntity
+import com.example.jikimi.databinding.CustomDialogBinding
 import com.example.jikimi.databinding.FragmentBottomSheetBinding
 import com.example.jikimi.databinding.FragmentLikeBottomSheetBinding
 import com.example.jikimi.presentation.VisibilityView
@@ -17,6 +22,11 @@ import com.example.jikimi.presentation.adapter.LikeAdapter
 import com.example.jikimi.viewmodel.LikeViewModel
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.snackbar.Snackbar
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraAnimation
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.NaverMap
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
@@ -30,7 +40,34 @@ class LikeBottomSheetFragment : BottomSheetDialogFragment() {
 
     private val likeAdapter : LikeAdapter by lazy {
         LikeAdapter(
+            onClick = { item, position ->
+                likeEntity = item
+                val likeEntity = LikeEntity(
+                    vtAcmdfcltyNm = likeEntity.vtAcmdfcltyNm,
+                    rnAdres = likeEntity.rnAdres,
+                    dtlAdres = likeEntity.dtlAdres,
+                    distanceData = likeEntity.distanceData,
+                    shelterType = likeEntity.shelterType,
+                    latitude = likeEntity.latitude,
+                    longitude = likeEntity.longitude
+                )
+                Toast.makeText(requireContext(), "$likeEntity", Toast.LENGTH_SHORT).show()
+            },
 
+            onLongClick = {item, positon ->
+                likeEntity = item
+
+                CustomDialog(likeEntity =
+                LikeEntity(
+                    vtAcmdfcltyNm = likeEntity.vtAcmdfcltyNm,
+                    rnAdres = likeEntity.rnAdres,
+                    dtlAdres = likeEntity.dtlAdres,
+                    distanceData = likeEntity.distanceData,
+                    shelterType = likeEntity.shelterType,
+                    latitude = likeEntity.latitude,
+                    longitude = likeEntity.longitude
+                ))
+            }
         )
     }
 
@@ -103,6 +140,24 @@ class LikeBottomSheetFragment : BottomSheetDialogFragment() {
                 }
             }
         }
+    }
+
+
+    private fun CustomDialog(likeEntity: LikeEntity){
+        val dialog = Dialog(requireContext())
+        val dialogBinding = CustomDialogBinding.inflate(layoutInflater)
+        dialog.setContentView(dialogBinding.root)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        dialogBinding.dialogCancelBtn.setOnClickListener {
+            dialog.dismiss()
+        }
+        dialogBinding.dialogDeleteBtn.setOnClickListener {
+            likeViewModel.deleteData(likeEntity.vtAcmdfcltyNm)
+            Snackbar.make(dialogBinding.dialogDeleteBtn, "좋아요가 삭제되었습니다.", Snackbar.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+        dialog.show()
     }
 
 
