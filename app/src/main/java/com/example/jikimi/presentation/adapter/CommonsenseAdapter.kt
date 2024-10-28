09609953b67382d00173af49ea1dce6dff382d04
wndnjs00 <juwonjeon852@gmail.com>
2024-10-28 15:59:46 +0900

    feat: 자연재난 로직 구현완료

--- app/src/main/java/com/example/jikimi/presentation/adapter/CommonsenseAdapter.kt
+++ app/src/main/java/com/example/jikimi/presentation/adapter/CommonsenseAdapter.kt
@@ -39,27 +39,29 @@ class CommonsenseAdapter(
             with(binding){
                 cardViewTv.text = item.safetyCateNm2
 
-                // 안전 카테고리와 이미지 매핑
-                val imageMap = mapOf(
-                    "태풍" to R.drawable.wind_img,
-                    "호우" to R.drawable.rain_img,
-                    "강풍" to R.drawable.storm_img,
-                    "대설" to R.drawable.snow_img,
-                    "한파" to R.drawable.cold_img,
-                    "풍랑" to R.drawable.storm,
-                    "황사" to R.drawable.sand_img,
-                    "폭염" to R.drawable.heat_img,
-                    "가뭄" to R.drawable.drought_img,
-                    "산사태" to R.drawable.landslide_img,
-                )
-
-                // 해당 카테고리에 맞는 이미지 가져오기, 기본 이미지는 ic_launcher_foreground
-                val image = imageMap[item.safetyCateNm2] ?: R.drawable.ic_launcher_foreground
+                val disasterImage = when (item.safetyCateNm2) {
+                    "태풍" -> R.drawable.wind_img
+                    "홍수" -> R.drawable.flood_img
+                    "호우" -> R.drawable.rain_img
+                    "강풍" -> R.drawable.storm_img
+                    "대설" -> R.drawable.snow_img
+                    "한파" -> R.drawable.cold_img
+                    "풍랑" -> R.drawable.tsunami_earthquake_img
+                    "황사" -> R.drawable.sand_img
+                    "폭염" -> R.drawable.heat_img
+                    "가뭄" -> R.drawable.drought_img
+                    "지진" -> R.drawable.earthquake_img
+                    "지진해일" -> R.drawable.storm
+                    "해일" -> R.drawable.tsunami_img
+                    "산사태" -> R.drawable.landslide_img
+                    "화산폭발" -> R.drawable.volcano_img
+                    else -> R.drawable.ic_launcher_foreground
+                }
 
                 //coil사용해서 이미지 띄우기
-                cardViewIv.load(item.contentsUrl){
-                    placeholder(image)  //로딩중
-                    error(image) // 에러발생시
+                cardViewIv.load(disasterImage){
+                    placeholder(R.drawable.ic_launcher_foreground)  //로딩중
+                    error(R.drawable.ic_launcher_foreground) // 에러발생시
                 }
             }
         }
