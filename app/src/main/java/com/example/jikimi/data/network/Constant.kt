043ae26601cee542c73de0df6d158f28317ab56b
wndnjs00 <juwonjeon852@gmail.com>
2025-04-17 13:32:18 +0900

    feat: 음성인식 기능구현

--- app/src/main/java/com/example/jikimi/data/network/Constant.kt
+++ app/src/main/java/com/example/jikimi/data/network/Constant.kt
@@ -28,8 +28,4 @@ object Constant{
     const val OUTDOOR_DISTANCE_DATA = "outdoor_distance_data"
     const val INDOOR_SHELTER_DATA = "indoor_shelter_data"
     const val INDOOR_DISTANCE_DATA = "indoor_distance_data"
-    const val ITEM_DATA = "item_data"
-    const val ITEMS_DATA = "items_data"
-
-    const val ALL_ITEMS = "all_items"
 }
